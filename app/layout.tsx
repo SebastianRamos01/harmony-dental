8bec95a707e769f14fd1d3fe02b4dea27fb0b4b3
SebastianRamos01 <sebasr.sr@hotmail.com>
2025-11-01 18:10:12 -0300

    more font bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const geistInter = Inter({
 });
 
 const arialLocalFont = localFont({
-  src: '../public/fonts/arial/arial.ttf',
+  src: '../public/fonts/arial.ttf'  ,
   display: 'swap',
   variable: '--font-arial'
 })
